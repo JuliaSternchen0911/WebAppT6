4f1e3a7fd43a2173c2af4cf8fcb6af4fe69780d1
MareikeHa <mareike.haffelder@gmail.com>
2024-02-27 16:21:34 +0100

    Dateien angelegt

--- index.js
+++
@@ -1,9 +0,0 @@
-
-// Überprüfen, ob ein Benutzer angemeldet ist
-firebase.auth().onAuthStateChanged(user => {
-    if (user) {
-        document.getElementById('userDiv').innerHTML = `Angemeldet als ${user.email}`;
-    } else {
-        document.getElementById('userDiv').innerHTML = 'Nicht angemeldet';
-    }
-});
